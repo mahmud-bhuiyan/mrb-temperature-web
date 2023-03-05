b86b3c8dc782070b33085633e27c09ab22887732
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-03-06 04:17:28 +0600

    dynamically data showed in html

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // api key
-
 const API_KEY = `c08d3862b801fd0bc8c52cd1ed89fb07
 `;
 
+//fetching data using api
 const loadTemperature = async (city) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   try {
@@ -14,10 +14,26 @@ const loadTemperature = async (city) => {
   }
 };
 
+//showing fetched data
 const displayTemperature = (data) => {
-  // console.log(data.main.temp);
-  const temperature = document.getElementById("temperature");
-  temperature.innerText = data.main.temp;
+  setInnerTextById("temperature", data.main.temp);
+  setInnerTextById("condition", data.weather[0].main);
+  // console.log(data.weather[0].main);
 };
+
+//set innerText function
+const setInnerTextById = (id, text) => {
+  const targetText = document.getElementById(id);
+  targetText.innerText = text;
+};
+
+//search city
+document.getElementById("btn-search").addEventListener("click", function () {
+  const searchField = document.getElementById("search-field");
+  const city = searchField.value;
+  // set city
+  setInnerTextById("city", city.toUpperCase());
+  loadTemperature(city);
+});
 
 loadTemperature("dhaka");
